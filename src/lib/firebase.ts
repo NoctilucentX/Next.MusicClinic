6b01fc1b22706ca48f6ee0cb207842fb9dce541c
Bryan Cortez <bancortez2@gmail.com>
2025-08-04 00:50:23 +0800

    v1.0.3

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,12 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCWZu1Y_c3MdLHhiUtawnu6yvFmbBz69C8",
-  authDomain: "sernan-music.firebaseapp.com",
-  projectId: "sernan-music",
-  storageBucket: "sernan-music.firebasestorage.app",
-  messagingSenderId: "1063108047597",
-  appId: "1:1063108047597:web:0fe8da4ee1a7e60f9312e4",
+  apiKey: "AIzaSyC81Ew72DXWEwYI9oZf91nk3IzuM2mDNG8",
+  authDomain: "smc-mngt-system-1.firebaseapp.com",
+  projectId: "smc-mngt-system-1",
+  storageBucket: "smc-mngt-system-1.firebasestorage.app",
+  messagingSenderId: "1045324121453",
+  appId: "1:1045324121453:web:66ed158f2be23f13c4c664",
+  measurementId: "G-PYCEHCFLDS",
 };
 
 const app = initializeApp(firebaseConfig);
